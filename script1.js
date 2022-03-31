6ef5ce78f6ecb3e6b102e7c9e86d009e297bb7d5
HJ-C <swch56@naver.com>
2022-03-31 19:26:59 +0900

    2

--- script1.js
+++ script1.js
@@ -10,8 +10,8 @@ const search = document.getElementById('search'),
 function searchMeal(e) {
 	e.prventDefault()
 
-	  // Clear single meal
-		single_mealEl.innerHTML = '';
+	// Clear single meal
+	single_mealEl.innerHTML = '';
 
 	//검색
 	const term = search.ariaValueMax
@@ -25,7 +25,7 @@ function searchMeal(e) {
         resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
 
         if (data.meals === null) {
-          resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
+          resultHeading.innerHTML = `<p>결과가 없습니다. 다시 이용해 주세요.<p>`;
         }else {
           mealsEl.innerHTML = data.meals
             .map(
@@ -38,56 +38,16 @@ function searchMeal(e) {
             </div>
           `
             )
-					.join('');
+			.join('');
         }
       });
     // Clear search text
     search.value = '';
   } else {
-    alert('Please enter a search term');
+    alert('단어를 입력하세요.');
   }
-}
-
-// 검색 ID 받아오기
-function getMealById(mealID){
-	fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-		.then(res => res.json())
-		.then(data => {
-			const meal = data.meals[0]
-
-			// addMealToDOM(meal);
-		})
-}
-
-// API 랜덤 받아오기
-function getRandomMeal(){
-	mealsEl.innerHTML = ''
-	resultHeading.innerHTML = ''
-
-	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-		.then(res=> res.json())
-		.then(data => {
-			const meal = data.meal[0]
-
-			// addMealToDOM(meal);
-		})
 }
 
 // Event Listener
 submit.addEventListener('submit',searchMeal )
 random.addEventListener('click', getRandomMeal )
-
-mealsEl.addEventListener('click', e=> {
-	const mealInfo = e.Path.find(item => {
-		if(item.calssList){
-			return item.calssList.contains('meal-info')
-		}else {
-			return false
-		}
-	})
-
-	if(mealInfo){
-		const mealId = mealInfo.getAttribute('data-mealid')
-		getMealById(mealId)
-	}
-})
